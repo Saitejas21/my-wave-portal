dbf62c4544202bce6411c4e7565e57abc9fe9b6f
Sai Tejas A R <46528244+Saitejas21@users.noreply.github.com>
2021-12-01 15:59:31 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,8 +4,8 @@ module.exports = {
   solidity: '0.8.0',
   networks: {
     rinkeby: {
-      url: 'https://eth-rinkeby.alchemyapi.io/v2/yDE3qh7fT9RMnUMlIucJEnS1oF-3CdeK',
-      accounts: ['ed48145e91ce388c82adef0f2643dcc63132c766292281a61c49af05fd6d9dc9'],
+      url: '-',
+      accounts: ['-'],
     },
   },
 };
